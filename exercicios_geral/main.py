606deae521823831ab1fb86dc82c4ebcdf4a7033
Rafael Dalmarco <111667173+rdalmarco@users.noreply.github.com>
2023-09-18 19:35:44 -0300

    18092023_2

---
+++ exercicios_geral/main.py
@@ -0,0 +1,4 @@
+print('exercicio')
+##Leitura número alunos e avaliações turma
+##Ler cada nota (p/ cada aluno) salvar arquivo (com nome)
+##Ler os dados (txt), calcular médias e apresentar resultados.
