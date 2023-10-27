de7a001368c33879627897773f7a295db0a99c10
Rafael Dalmarco <111667173+rdalmarco@users.noreply.github.com>
2023-10-26 23:02:17 -0300

    26102023 - gera instancias 65% pronto

--- problema_sessoes_eleitorais/Locais.py
+++ problema_sessoes_eleitorais/Locais.py
@@ -8,19 +8,21 @@ class Locais:
         inst_locais = []
         i = 0
         while (num_inst_locais < num_locais):
-           if(i < 24):
-            i += 1
-            class_setor = class_setores['chave' + str(i + 1)]['valor']
-            if (class_setor == 'urb'):
-              random = r.random() < 0.35
-              if (random == 1):
-                inst_locais.append(i + 1)
-                num_inst_locais += 1
-              else:
-               random = r.random() < 0.10
-               if (random == 1):
-                  inst_locais.append(i + 1)
-                  num_inst_locais += 1
-            else :
+           if(i < len(class_setores)):
+                i += 1
+                class_setor = class_setores['chave' + str(i + 1)]['valor']
+                if (class_setor == 'urb'):
+                  random = r.random() < 0.35
+                  if (random == 1):
+                    inst_locais.append(i + 1)
+                    num_inst_locais += 1
+                    class_setores.pop('chave' + str(i + 1))
+                else:
+                   random = r.random() < 0.10
+                   if (random == 1):
+                      inst_locais.append(i + 1)
+                      num_inst_locais += 1
+                      class_setores.pop('chave' + str(i + 1))
+           else :
                 i = 0
         return inst_locais
